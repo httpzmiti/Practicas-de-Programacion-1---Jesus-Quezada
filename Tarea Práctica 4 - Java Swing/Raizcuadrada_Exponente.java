680d91099641fe6cc55a6492e215378be1edfd94
Rainiel de los Santos <124511195+httpzmiti@users.noreply.github.com>
2026-03-20 22:52:47 -0400

    Raizcuadrada_Exponente clase

---
+++ Tarea Práctica 4 - Java Swing/Raizcuadrada_Exponente.java
@@ -0,0 +1,10 @@
+public class Raizcuadrada_Exponente {
+
+    public double raiz(double num) {
+        return Math.sqrt(num);
+    }
+
+    public double potencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
+}
